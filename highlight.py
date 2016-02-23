8b753a8ea7c81fc6cc103c5910bb21c21c659279
mfrancis95 <mikefrancis95@gmail.com>
2016-02-23 09:30:57 -0500

    Add --file argument for reading from files instead of stdin

--- highlight.py
+++ highlight.py
@@ -1,9 +1,14 @@
 from json import load
+from argparse import ArgumentParser
 from re import sub
 import sys
 
 with open("jsonPatterns.json") as f:
     patterns = load(f)
+
+parser = ArgumentParser("highlight")
+parser.add_argument("-f", "--file")
+args = parser.parse_args()
 
 colors = {
     "default": "\033[39m",
@@ -13,7 +18,15 @@ colors = {
 }
 colorize = lambda match, color: colors[color] + sub("\033\[..m", "", match.group()) + colors["default"]
 
-for line in sys.stdin:
-    for pattern in patterns:
-        line = sub(pattern["pattern"], lambda match: colorize(match, pattern["color"]), line)
-    sys.stdout.write(line)
+def highlight(file):
+    for line in file:
+        for pattern in patterns:
+            line = sub(pattern["pattern"], lambda match: colorize(match, pattern["color"]), line)
+        sys.stdout.write(line)
+
+file = args.file
+if file:
+    with open(file) as f:
+        highlight(f)
+else:
+    highlight(sys.stdin)
